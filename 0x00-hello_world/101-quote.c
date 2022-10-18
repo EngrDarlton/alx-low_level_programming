0ae8fee1f0c30c8769d83319b37d21ff6bd33138
Agajere Darlton <agajere.darlton@gmail.com>
2022-10-17 19:08:15 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Description: prints with printf "P"
+ * Return: 0
+ */
+
+
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
