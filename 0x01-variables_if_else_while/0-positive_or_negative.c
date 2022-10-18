14708cfdca619e299d5c0b4d8f97f8facf9b5f3c
Agajere Darlton <agajere.darlton@gmail.com>
2022-10-17 19:41:46 -0700

    Compare n with zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 	/* your code goes there */
 	if (n < 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is possitive\n", n);
 	}
 	return (0);
 
