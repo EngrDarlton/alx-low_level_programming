b13ee0f21e14d25f9d0de077a493d359d49bba28
Agajere Darlton <agajere.darlton@gmail.com>
2022-10-20 18:11:41 -0700

    Uper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: Print upper and lower cases
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int ah;
+
+	for (ah = 'a'; ah <= 'z'; ah++)
+	{
+		putchar(ch);
+	}
+	for (ah = 'A'; ah <= 'Z'; ah++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
