2a263f664b300654e3959695ff414a08d3f6dec4
Agajere Darlton <agajere.darlton@gmail.com>
2022-10-20 18:44:20 -0700

    1 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: Print 1  to 10
+ * Return: 0 Success
+*/
+
+int main(void)
+{
+	int a;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	putchar('\n');
+
+	return (0);
+}
