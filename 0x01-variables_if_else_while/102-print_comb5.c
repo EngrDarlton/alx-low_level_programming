3ef2974efa162fc617f3e592928dc5193b516bce
Agajere Darlton <agajere.darlton@gmail.com>
2022-10-23 15:58:03 -0700

    Combination of 2digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry points
+ * Description: Prints combination of2 digit numbers
+ * Return: Always (Success)
+*/
+int main(void)
+{
+	int x, y, z, a;
+
+	for (x = '48'; x <= '57'; x++)
+	{
+		for (y = '48'; y <= '57'; y++)
+		{
+			for (z = '48'; z <= '57'; z++)
+			{
+				for (a = 48; a <= 57; a++)
+				{
+				if (((z + a) > (x + y) && z >= x) || x < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(' ');
+					putchar(z);
+					putchar (a);
+
+					if (x + y + z + a == 227 && x == 57)
+					{
+					break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
